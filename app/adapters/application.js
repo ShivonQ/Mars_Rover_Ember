b04d21ecf5fbc75ce64964dbc5755ca4df15101d
ShivonQ <realitykeeper@gmail.com>
2017-03-15 12:17:21 -0500

    fixed typo in adater

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -2,7 +2,7 @@ import DS from 'ember-data';
 import Ember from 'ember'
 import JSONAPIAdapter from 'ember-data/adapters/json-api';
 
-const { String: {pluralize, understcore}} = Ember;
+const { String: {pluralize, underscore}} = Ember;
 
 
 export default JSONAPIAdapter.extend({
