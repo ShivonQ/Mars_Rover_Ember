4c331ba81be39a1a62ec7d76dbdb28434367b036
ShivonQ <realitykeeper@gmail.com>
2017-03-15 13:26:07 -0500

    Fixewd missing attr in model fave_img

--- app/models/fave-img.js
+++ app/models/fave-img.js
@@ -2,6 +2,7 @@ import DS from 'ember-data';
 
 export default DS.Model.extend({
   rover: DS.attr('string'),
+  cam_name:DS.attr('string'),
   imageDate: DS.attr('string'),
   url: DS.attr('string'),
   user: DS.belongsTo('user')
