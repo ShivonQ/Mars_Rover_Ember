2c80894c840a59eef864087df04971dcf1f39117
ShivonQ <realitykeeper@gmail.com>
2017-03-15 13:28:04 -0500

    Typo fave-img model

--- app/models/fave-img.js
+++ app/models/fave-img.js
@@ -3,7 +3,7 @@ import DS from 'ember-data';
 export default DS.Model.extend({
   rover: DS.attr('string'),
   cam_name:DS.attr('string'),
-  imageDate: DS.attr('string'),
+  image_date: DS.attr('string'),
   url: DS.attr('string'),
   user: DS.belongsTo('user')
 });
