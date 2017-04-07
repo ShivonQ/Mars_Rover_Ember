efc61ca35df2fef3913c415eaf13894c8833031e
ShivonQ <realitykeeper@gmail.com>
2017-04-06 22:47:02 -0500

    Sorted Rover bio Pages routes, forgot my ':' on the params in the route

--- app/router.js
+++ app/router.js
@@ -7,14 +7,14 @@ const Router = Ember.Router.extend({
 
 Router.map(function() {
   this.route('rover_manifests',{path: '/'});
-  this.route('user', {path:'/user/user_id'});
-  this.route('rover_manifest', {path: '/rover_manifest/rover_manifest_id'});
+  this.route('user', {path: '/user/:user_id'});
+  this.route('rover_manifest', {path: '/rover_manifest/:rover_manifest_id'});
   this.route('fave_img');
   this.route('image');
   this.route('camera');
 
   this.route('rover-manifests');
-    this.route('rover-manifest');
+    this.route('rover_manifest');
 });
 
 export default Router;
